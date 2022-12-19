5e532d08b0af3b3e38a940cf4d9994b16b3a1c99
Teemu <Teemu.eloranta@student.lut.fi>
2022-12-19 12:36:23 +0200

    Eka toimii

---
+++ dist/index.js
@@ -0,0 +1,17 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const app = (0, express_1.default)();
+const port = 5000;
+app.get("/", (req, res) => {
+    res.send("Hello from TS-Express application – I am up and Kukkuuu!!");
+});
+app.listen(port, () => {
+    console.log("Server is up'n'running at http://localhost:" + port);
+});
+app.get("/hello", (req, res) => {
+    res.send("Hello world");
+});

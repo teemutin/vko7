4a0fcec20431479149a9bc8e1c495fdbc9585d19
Teemu <Teemu.eloranta@student.lut.fi>
2022-12-19 12:41:11 +0200

    Oikee portti

--- index.ts
+++ index.ts
@@ -1,7 +1,7 @@
 import express, {Express, Request, Response} from "express"
 
 const app: Express = express()
-const port: number = 5000
+const port: number = 3000
 
 
 app.get("/", (req: Request, res: Response) => {
